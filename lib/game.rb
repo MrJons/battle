fa398b1b4920633d5d9ea19cb77c20d4abe019ab
MrJons <shad_stick@hotmail.co.uk>
2016-09-02 15:27:09 +0100

    basic game finished. no interface structure included.

--- lib/game.rb
+++ lib/game.rb
@@ -2,17 +2,23 @@
 
 class Game
 
-  attr_reader :player1, :player2, :current_player, :other_player
+  attr_reader :player1, :player2, :current_player, :other_player, :winner, :looser
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @current_player = @player2
     @other_player = @player1
+    @winner = nil
+    @looser = nil
   end
 
   def attack
     @other_player.be_attacked
+    if @other_player.hit_points <= 0
+      @looser = @other_player
+      @winner = @current_player
+    end
   end
 
   def switch_turns
